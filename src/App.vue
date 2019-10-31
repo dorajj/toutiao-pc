47ae287f49c98f8b7160d6eed7230de7ab727621
wangjiajia <2116326418@qq.com>
2019-10-31 14:44:13 +0800

    登录模块-基础布局

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    toutiao <el-button type="primary">主要按钮</el-button>
+    <!-- 一级路由的容器 -->
+    <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }

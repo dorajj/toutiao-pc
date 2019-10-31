56019e572e4cb5045d5e55e2bf5ee0aeed2afc2c
wangjiajia <2116326418@qq.com>
2019-10-31 19:53:10 +0800

    登录模块-绘制表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,12 +4,37 @@
     <!-- 卡片容器  el-card是一个element组件，根元素上默认添加一个类和组件的名称一致 -->
     <el-card>
       <img src="../../assets/logo_index.png" alt />
+      <!-- 表单 -->
+      <el-form :model="LoginForm">
+        <el-form-item>
+          <el-input v-model="LoginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="LoginForm.code" placeholder="请输入验证码" style="width:240px;margin-right:8px"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit" style="width:100%;">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      LoginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
@@ -34,7 +59,7 @@ export default {}
     img {
       width: 200px;
       display: block;
-      margin: 0 auto;
+      margin: 0 auto 30px;
     }
   }
 }

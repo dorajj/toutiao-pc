dfebfdf418460a7730877f333fc53063ba503397
wangjiajia <2116326418@qq.com>
2019-11-07 21:23:12 +0800

    登录模块-校验和登录

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+// 1. 导入某一个目录，默认会去找索引文件（index.js index.vue index.json） 基于webpack
+// 2. @ 别名  指定的是 /src 路径  一个绝对路径。 基于webpack
 import router from '@/router'
+
+import axios from '@/api'
+Vue.prototype.$http = axios
 
 Vue.use(ElementUI)
 

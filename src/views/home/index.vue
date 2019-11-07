829b0c61f961ca82ad65c658061fc4955acb8aa9
wangjiajia <2116326418@qq.com>
2019-11-07 21:44:47 +0800

    首页模块-头部内容

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,24 @@
   <el-container class="home-container">
     <el-aside width="200px">Aside</el-aside>
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <!-- 图标 -->
+        <span class="el-icon-s-fold icon"></span>
+        <!-- 文字 -->
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <!-- 下拉菜单组件 -->
+        <el-dropdown class="dropdown">
+          <span class="el-dropdown-link">
+            <img class="headIcon" src="../../assets/avatar.jpg" alt />
+            <span class="userName">用户名</span>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -24,6 +41,28 @@ export default {}
   }
   .el-header {
     border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      font-size: 30px;
+      vertical-align: middle;
+    }
+    .text {
+      margin-left: 10px;
+      vertical-align: middle;
+    }
+    .dropdown {
+      float: right;
+      .headIcon {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .userName {
+        margin-left: 5px;
+        font-weight: bold;
+        vertical-align: middle;
+      }
+    }
   }
 }
 </style>
